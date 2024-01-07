3177bfa350c8083efd0273ab6ef5f06325a8bc69
Ujjwal Pandey <ujjwalpandey922@gmail.com>
2024-01-07 22:16:17 +0530

    change cors origin

--- Server/index.js
+++ Server/index.js
@@ -7,11 +7,13 @@ const app = express();
 // Set the port for the server
 const PORT = process.env.PORT || 5000;
 // Add this line to enable CORS for all routes
-app.use(
-  cors({
-    origin: "https://currency-convertor-backend.vercel.app",
-  })
-);
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://currency-convertor-jet.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
 // Start the server and log a message when it's running
 const server = app.listen(PORT, () => {
   console.log(`App Running on Port ${PORT}`);
